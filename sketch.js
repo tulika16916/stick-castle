e7009c5d0917c96a3d7271a7855777b3b020f775
tulika16916 <63865540+tulika16916@users.noreply.github.com>
2020-05-25 19:37:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(800,400);
+  
+}
+function draw() {
+ background("yellow")
+ rect(250,200,300,300) 
+ rect(100,150,150,350)
+ rect(500,150,150,350)
+ rect(60,50,50,400)
+ rect(650,50,50,400)
+}
